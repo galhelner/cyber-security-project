0e8ce8cf6d8d3bb4157752899a5cb396c0a00d49
Gal Helner <galhelner@gmail.com>
2025-09-01 12:40:24 +0300

    added profile page mock design

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
 export default function Profile() {
   return (
     <div className="flex flex-col flex-1 items-center">
-        <h1 className='text-4xl'>Profile</h1>
+      <div className="flex flex-col items-center mt-10 p-10 bg-gradient-to-br from-gray-50 to-white rounded-2xl shadow-xl border border-gray-200">
+         <h1 className='text-3xl pb-5 font-extrabold text-blue-800'>Gal Helner</h1>
+        <Image src="/profile-logo.png" alt="Profile Image" width={200} height={200} className="rounded-full mt-4"/>
+      </div>
     </div>
   );
 }
